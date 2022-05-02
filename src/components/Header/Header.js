5d3b3e589879b256cd7e29f03968409fdba4b082
Justin Muir <justinmuir@gmail.com>
2022-05-02 18:47:37 -0400

    styled mobile header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,6 @@
 import logo from '../../assets/images/logos/BrainFlix-logo.svg';
+import searchImg from '../../assets/images/icons/search.svg';
+import uploadImg from '../../assets/images/icons/upload.svg';
 import './Header.scss';
 
 const Header = () => {
@@ -7,16 +9,14 @@ const Header = () => {
             <div className='header__logo'><img src={logo} alt='Brainflix logo'/></div>
             <div className='header__right'>
                 <div className='header__search-container'>
-                    <img src="../../assets/images/icons/search.svg"/>
+                    <img src={searchImg}/>
                     <input type='text' placeholder='Search'></input> 
                 </div>
-                <input type='submit' value='submit' className='header__upload-button'>
-                    <img src="../../assets/images/icons/upload.svg"/>
-                    UPLOAD
-                </input>
-                <div className='header__image'>
-                     <img src="../../assets/images/images/Mohan-muruge.jpg"/>
-                </div>
+                <button type='submit' value='submit' className='header__upload-button'>
+                    <img src={uploadImg}/>
+                    <p>UPLOAD</p>
+                </button>
+                <div className='header__image'></div>
             </div>
         </div>
     </>
