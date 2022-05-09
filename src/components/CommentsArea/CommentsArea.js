49032a0c935bbbb29db049075065c7e2fcb1a247
Justin Muir <justinmuir@gmail.com>
2022-05-09 10:08:40 -0400

    adding comment area

--- src/components/CommentsArea/CommentsArea.js
+++ src/components/CommentsArea/CommentsArea.js
@@ -4,12 +4,26 @@ import Comment from '../Comment/Comment';
 const CommentsArea = (props) => {
     return (<>
         <section className='comments__container'>
-            {props.mainVideo.comments.map((content) => <Comment 
-                dateMaker={props.dateMaker} 
-                name={content.name} 
-                timestamp={content.timestamp} 
-                comment={content.comment}
-                />)}
+            
+            
+            <div className="comments__input-area">
+                <div className="comments__image"></div>
+                <form className="comments__form" id="comments-form">
+                    <label for="comment">JOIN THE CONVERSATION</label>
+                    <textarea name="comment" className="form-input" id="comment" placeholder="Add a new comment"></textarea>
+                    <div className="btn-container">
+                        <button type="submit" id="submit">COMMENT</button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                {props.mainVideo.comments.map((content) => <Comment 
+                    dateMaker={props.dateMaker} 
+                    name={content.name} 
+                    timestamp={content.timestamp} 
+                    comment={content.comment}
+                    />)}
+            </div>
         </section>
     </>
     )
