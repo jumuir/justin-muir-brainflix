6545c73576693ca016c224764819804ab615b260
Justin Muir <justinmuir@gmail.com>
2022-05-11 22:55:00 -0400

    Tablet formatting complete

--- src/components/CommentsArea/CommentsArea.js
+++ src/components/CommentsArea/CommentsArea.js
@@ -5,12 +5,15 @@ const CommentsArea = (props) => {
     return (<>
         <section className='comments__container'>
             
+            <p className='comments__count'>{props.mainVideo.comments.length} Comments</p>
             
             <div className="comments__input-area">
                 <div className="comments__image"></div>
                 <form className="comments__form" id="comments-form">
-                    <p className='comments__form-title'>JOIN THE CONVERSATION</p>
-                    <textarea name="comment" className="form-input" id="comment" placeholder="Add a new comment"></textarea>
+                    <div className='comments__form-left'>    
+                        <p className='comments__form-title'>JOIN THE CONVERSATION</p>
+                        <textarea name="comment" className="comments__form-input" id="comment" placeholder="Add a new comment"></textarea>
+                    </div>
                     <div className="btn-container">
                         <button type="submit" id="submit">
                             <p>COMMENT</p>
