cfe48bfb9af663eaa47beeae382de2e9cec6a06b
Justin Muir <justinmuir@gmail.com>
2022-05-12 23:28:55 -0400

    added video controls

--- src/components/VideoPlayer/VideoPlayer.js
+++ src/components/VideoPlayer/VideoPlayer.js
@@ -1,4 +1,10 @@
 import './VideoPlayer.scss';
+import playImg from '../../assets/images/icons/play.svg';
+import pauseImg from '../../assets/images/icons/pause.svg';
+import fullscreenImg from '../../assets/images/icons/fullscreen.svg';
+import closeImg from '../../assets/images/icons/close_fullscreen.svg';
+import volumeImg from '../../assets/images/icons/volume_up.svg';
+import muteImg from '../../assets/images/icons/volume_off.svg';
 
 
 const VideoPlayer = (props) => {
@@ -6,6 +12,14 @@ const VideoPlayer = (props) => {
 
     return (<>
         <section className='video-container'>
+            <div className='video-controls'>
+                <img className='video-button' src={playImg}/>
+                <img className='video-button' src={pauseImg}/>
+                <img className='video-button' src={fullscreenImg}/>
+                <img className='video-button' src={closeImg}/>
+                <img className='video-button' src={volumeImg}/>
+                <img className='video-button' src={muteImg}/>
+            </div>
             <video controls poster={mainVideo.image}>
 
             </video>
