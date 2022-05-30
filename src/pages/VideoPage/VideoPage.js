0df05810f92ba21caa304f2ce6ed0a31cd0141f5
Justin Muir <justinmuir@gmail.com>
2022-05-30 11:43:31 -0400

    Added routing to video page

---
+++ src/pages/VideoPage/VideoPage.js
@@ -0,0 +1,62 @@
+import { Component } from 'react';
+import Header from '../../components/Header/Header';
+import VideoPlayer from '../../components/VideoPlayer/VideoPlayer';
+import VideoInfo from '../../components/VideoInfo/VideoInfo';
+import VideosList from '../../components/VideosList/VideosList';
+import CommentsArea from '../../components/CommentsArea/CommentsArea';
+import videoDetails from '../../data/video-details.json';
+import './VideoPage.scss';
+
+
+class VideoPage extends Component {
+  state = {
+    mainVideo: videoDetails[0],
+    videoDetails: videoDetails
+  }
+
+  changeMainVideo = (videoId) => {
+    const newMainVideo = this.state.videoDetails.filter(details => details.id === videoId);
+    this.setState({ mainVideo:newMainVideo[0] });
+  }
+
+  addComment = (comment) => {
+    let newDetails = [...videoDetails]
+    let now = new Date().getTime();
+    newDetails.forEach((vid, index) => {
+      if (vid.id === this.state.mainVideo.id) {
+        const comms = newDetails[index].comments;
+        let newComms = [{
+          name: 'Anonymous',
+          comment: comment,
+          likes: 0,
+          timestamp: now
+        },...comms];
+        
+        newDetails[index].comments = newComms;
+        this.setState({ videoDetails: newDetails});
+        
+      }
+    });
+  }
+
+
+  render() {
+    return (
+      <>
+      <Header />
+      <VideoPlayer mainVideo={this.state.mainVideo} />
+      <div className='desktop-columns'>
+        <div className='desktop-columns__left'>
+          <VideoInfo mainVideo={this.state.mainVideo}/>
+          <CommentsArea mainVideo={this.state.mainVideo} addComment={this.addComment}/>
+        </div>
+        <div className='desktop-columns__right'>
+          <VideosList mainVideo={this.state.mainVideo} changeMainVideo={this.changeMainVideo}/>
+        </div>
+      </div>
+      </>
+    );
+  }
+}
+
+export default VideoPage;
